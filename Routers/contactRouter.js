252f27d84e3d4ce96f6bd89dc1a65e7b18a04aee
Bryce-Soghigian <brybrycodingguy@gmail.com>
2020-11-03 02:50:33 -0600

    got phone endpoint functionality working

--- Routers/contactRouter.js
+++ Routers/contactRouter.js
@@ -1,66 +1,70 @@
-const router = require('express').Router();
-const database = require("../config/knex-config")
+const router = require("express").Router();
+const database = require("../config/knex-config");
 const nodemailer = require("nodemailer");
-require('dotenv').config();
+require("dotenv").config();
 
-  /**
-   * phone endpoint
-   * The phone endpoint is much simpler
-   * it will just send my phone number to the recipient and the date and time via email
-   * So req.body = {email:"",'${time}':"Event","currentDate":"01012020"}
-   * and update my current schedule withreq.body = {state.currentTime.time:`Phone meeting at ${state.currentTime.time}`
-   */
-router.put("/phone", (req,res) => {
-const date = req.body.currentDate
-const email = req.body.email
 
-    //Set up the update object
-    let key;
-    let updateObject = {};
-    var hasNumber = /\d/;
-    let keysArray = Object.keys(req.body);
-    keysArray.map(x => {
-        if(hasNumber.test(x)){
-            key = x
-        }
-    })
-    updateObject[key] = req.body[key]
-    database("day")
-    .where("day.currentDate",date)
+/**
+ * phone endpoint
+ * The phone endpoint is much simpler
+ * it will just send my phone number to the recipient and the date and time via email
+ * So req.body = {email:"",'${time}':"Event","currentDate":"01012020"}
+ * and update my current schedule withreq.body = {state.currentTime.time:`Phone meeting at ${state.currentTime.time}`
+ */
+router.put("/phone", (req, res) => {
+  const monthsArray = ["Jan","Feb","Mar","Apr","May","Jun","Jul","Aug","Sep","Oct","Nov","Dec"]
+  const date = req.body.currentDate;
+  const email = req.body.email;
+  const currentDate = req.body.currentDate;
+  let month = monthsArray[currentDate.slice(0,2)-1]
+  let day = currentDate.slice(2,4)
+  //Set up the update object
+  let key;
+  let updateObject = {};
+  let responseObject = {};
+  var hasNumber = /\d/;
+  let keysArray = Object.keys(req.body);
+  keysArray.map((x) => {
+    if (hasNumber.test(x)) {
+      key = x;
+    }
+  });
+  updateObject[key] = req.body[key];
+  const mailOptions = {
+    from: "mrsoghigiansvirtualassistant@gmail.com", // sender
+    to: email, // receiver
+    subject: "Phone Interview", // Subject
+    html: `You have successfully scheduled a phone interview with Mr. Bryce Soghigian for ${key} on  ${month} ${day} \n Please call him at 515-361-2386`, // html body
+  };
+  const transport = nodemailer.createTransport({
+    service: "gmail",
+    auth: {
+      user: "mrsoghigiansvirtualassistant@gmail.com", //your gmail account you used to set the project up in google cloud console"
+      pass:process.env.emailPassword
+    },
+  });
+  database("day")
+    .where("day.currentDate", date)
     .update(updateObject)
     .returning("*")
-    .then(data => {
-    res.status(200).json({data:data,email:email})
+    .then((data) => {
+      responseObject["currentDay"] = data;
+    }).catch(err => {
+        console.log(err)
     })
+  transport.sendMail(mailOptions, function (err, result) {
+    if (err) {
+      res.send({
+        message: err,
+      });
+    } else {
+      transport.close();
+      res.send({
+        message: `You have successfully scheduled a phone interview with Mr. Bryce Soghigian for ${key} on  ${month} ${day} `,
+        data:responseObject
+      });
+    }
+  })
+});
 
-        // database("day")
-        //  .update({req.body.time:})
-
-        // let matrix  = Object.entries(req.body)
-        // const message = `Thanks for scheduling a meeting! \n you scheduled a phone meeting on ${req.body.currentDate} at ${matrix[1][0]} \n`
-        // const transporter = nodemailer.createTransport({
-        //     service:"gmail",
-        //     auth :{
-        //         user:"mrsoghigiansvirtualassistant@gmail.com",
-        //         pass: "Testpassword123"
-        //     }
-        // })
-        // const mailOptions = {
-        //     from:"mrsoghigiansvirtualassistant@gmail.com",
-        //     to:`req.body.email`,
-        //     subject:"Meeting with Bryce Soghigian",
-        //     text: message
-        // }
-        // transporter.sendMail(mailOptions,(error, info) => {
-        //     if(error){
-        //         console.log(error)
-        //     }else{
-        //         console.log(`Email sent: ${info.response}`)
-        //     }
-        // })
-        // return res.status(200).json(message)
-    
-})
-
-
-module.exports = router
+module.exports = router;
